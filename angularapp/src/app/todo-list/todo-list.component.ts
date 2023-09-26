00164c959529e0af4eb489ca93c48c1c58db0584
Iamneo <techadmin@iamneo.ai>
2023-09-26 06:24:30 +0000

    Backup Commit

---
+++ angularapp/src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements OnInit {
+  todos: Todo[] = [];
+  newTodo: string = '';
+
+  selectedTodo: Todo | null = null; // For editing
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  addTodo() {
+    if (this.newTodo.trim() !== '') {
+      this.todos.push({ text: this.newTodo, editing: false });
+      this.newTodo = '';
+    }
+  }
+
+  removeTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
+
+  editTodo(todo: Todo) {
+    this.selectedTodo = todo;
+    todo.editing = true;
+  }
+
+  cancelEdit(todo: Todo) {
+    this.selectedTodo = null;
+    todo.editing = false;
+  }
+
+  saveEdit(todo: Todo) {
+    todo.editing = false;
+    this.selectedTodo = null;
+  }
+}
+
+interface Todo {
+  text: string;
+  editing: boolean;
+}
+
